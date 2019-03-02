3a188a2c253bb4ef843f4696b86f3fd45f7d4136
Jesse Smokovich <jessesmokovich@yahoo.com>
2019-03-02 04:55:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,6 +18,9 @@ end
 def find_the_cheese(strings)
   i = 0
   cheese_types = ["cheddar", "gouda", "camembert"]
-  
+   while i < cheese_types.length
+    return cheese_types[i] if array.include?(cheese_types[i])
+    i += 1
+  end
 end
 
