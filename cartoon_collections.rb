3f8014aa96a40982ff5748c7778d4c6036cb4794
Jesse Smokovich <jessesmokovich@yahoo.com>
2019-03-02 03:52:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,8 @@ end
   
 
 def long_planeteer_calls(calls)
-  calls.find_all{|name| if name.length > 4 true}
+  calls.find_all{|name| if name.length > 4}
+  return true
 end
 
 def find_the_cheese# code an argument here
