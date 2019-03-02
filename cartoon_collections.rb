f56fe716ab60b517d773f00bf9ba1b63fe9e8796
Jesse Smokovich <jessesmokovich@yahoo.com>
2019-03-02 04:57:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,11 +16,6 @@ def long_planeteer_calls(planeteers)
 end
 
 def find_the_cheese(strings)
-  i = 0
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  while i < cheese_types.length
-    return cheese_types[i] if string.include?(cheese_types[i])
-    i += 1
-  end
+ 
 end
 
